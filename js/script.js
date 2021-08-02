e58fc76463e80dcc3e36ac217351329dcf59502d
ojedam13 <m_ojeda17@hotmail.com>
2021-08-02 10:23:10 -0300

    cambios css responsive

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// var nombreUsuario = prompt('Ingresa tu nombre');
+// var edad =prompt ('Ingresa tu edad');
+// var tatuaje = prompt ('¿Que tamaño de tatuaje te gustan (s,x,xl)?')
+
+// alert('Bienvenido ' + nombreUsuario);
