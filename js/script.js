0548d0df6f34c0b62534d56b48ea111a61e4c2ba
ojedam13 <m_ojeda17@hotmail.com>
2021-08-02 18:20:04 -0300

    cambios en js y html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-// var nombreUsuario = prompt('Ingresa tu nombre');
-// var edad =prompt ('Ingresa tu edad');
-// var tatuaje = prompt ('¿Que tamaño de tatuaje te gustan (s,x,xl)?')
+var nombreUsuario = prompt('Ingresa tu nombre');
+var edad = prompt('Ingresa tu edad');
+var gustan = prompt ('¿Te has tatuado alguna vez?')
+var tamaño = prompt('¿Qué tamaño de tatuaje te gustan (pequeño, mediano o grande)?');
+var estilo = prompt('¿Qué estilo de tatuaje deseas (Blackwork, Tradicional o Realista?');
 
-// alert('Bienvenido ' + nombreUsuario);
+
