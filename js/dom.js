b38b85ca99379a1c86c6afb63dc80624da94f084
ojedam13 <m_ojeda17@hotmail.com>
2021-08-09 17:33:51 -0300

    ultimos cambio de la unidad viii

--- js/dom.js
+++ js/dom.js
@@ -15,23 +15,10 @@ function mostrar() {
     
     var txt = document.getElementById('txt-artista');
     txt.style.opacity = '1';
+    
 
 }
     function ocultar() {
         var txt2 = document.getElementById('txt-artista');
         txt2.style.opacity = '0';
 }
-
-// mostrar img
-
-// function scroll() {
-//     var img = document.getElementById('img_trabajo');
-//     img.style.opacity = '1';
-
-// }
-
-// function noScroll() {
-//     var img2 = document.getElementById('img_trabajo');
-//     img2.style.opacity = '0';
-// }
-
